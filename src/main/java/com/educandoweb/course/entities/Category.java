aea5f585bfe6b7bd0bc6a4d9cee8263dfb3110e3
Daniel Ramos <danielexpeditospr2@gmail.com>
2025-12-01 18:39:30 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
@@ -15,7 +17,11 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    public Category(){
+    @Transient
+    private Set<Product> products = new HashSet<>();
+
+
+    public Category() {
 
     }
 
@@ -38,6 +44,14 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
     }
 
     @Override
